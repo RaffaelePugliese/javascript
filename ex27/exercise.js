99a69fe256d5206b657cebc2b2f17fc572e638bd
Raffaele <puglieseraffaele51@gmail.com>
2023-11-03 17:11:22 +0100

    new file

---
+++ ex27/exercise.js
@@ -0,0 +1,14 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const id = person.id;
+const personInfo = { 
+ ...person
+};
+delete personInfo.id
+
+console.log(id, personInfo);

b2bbb475852ab21e5d63c76baa84eb80071674b5
RaffaelePugliese <145784120+RaffaelePugliese@users.noreply.github.com>
2023-11-03 16:45:11 +0100

    Update exercise.js

--- ex25/exercise.js
+++ ex25/exercise.js
@@ -1,1 +1,8 @@
+let newNumber = [3];
+let numberStore = [0, 1, 2,...newNumber];
 
+// numberStore.push(newNumber);
+// function makeNewArray(newNumber){
+//  return [...newNumber,numberStore];
+// }
+console.log(numberStore);

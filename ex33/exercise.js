9b10afe197dbba4b67a1b8d9a8ce978d86958742
Raffaele <puglieseraffaele51@gmail.com>
2023-11-12 17:46:51 +0100

    new file

--- ex33/exercise.js
+++
@@ -1,8 +0,0 @@
-function printAsyncName(callback,string){
-   let greetings= setInterval(callback,1000);
-   setTimeout(()=>{clearInterval(greetings),console.log(string)},2000) 
- 
- 
-}
-
-printAsyncName(()=>console.log("Hello"),"Raffaele")

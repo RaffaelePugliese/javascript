a13a5f6e7d57855eeb0a92e29a880bfbd5e8c4f5
Raffaele <puglieseraffaele51@gmail.com>
2023-11-14 11:16:13 +0100

    ex37

--- ex34/exercise.js
+++
@@ -1,8 +0,0 @@
-function repeatHello(callback){
-   return setInterval(callback,1000)
-   
-}
-
-repeatHello(()=>console.log("hello"))
-// L'arrow function viene utilizzata nelle funzioni semplici per ridurre le righe di codice
-// e renderlo più intuitivo 
--- ex35/exercise.js
+++
@@ -1,6 +0,0 @@
-function repeatHello(callback){
-    let sayHi= setInterval(callback,5000)
-    setTimeout(()=>{clearInterval(sayHi)},5000)
- }
- 
- repeatHello(()=>console.log("hello"))

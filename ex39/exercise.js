4d2371d8a8a8f9bbe504e89febe9a4ec21f0a4a4
RaffaelePugliese <145784120+RaffaelePugliese@users.noreply.github.com>
2023-11-17 16:14:29 +0100

    Update exercise.js

--- ex39/exercise.js
+++ ex39/exercise.js
@@ -1,10 +1,6 @@
 async function listToDos(url){
     let response = await fetch(url,{
-        method:"POST",
-        body: JSON.stringify({
-            title:'Succes',
-            complited:true,
-        })
+    
     })
     let datas = await response.json();
     return datas
